c80d3881a506b1560534a97178c58bf3ee8ea5e6
fasfadad <shaoxuan_1999@hotmail.com>
2023-04-09 19:05:27 +0800

    testingggg

--- testinggggg.py
+++ testinggggg.py
@@ -33,7 +33,7 @@ def main_screen(root):
         23: {"photo" : "panpacific.png", "name" : "Amara Sanctuary Resort Sentosa","location" : "Sentosa","price" : 211,"hotelclass" : 5,"guestreview" : 7.6,"shuttlebusservice" : 1,"spa" : 1,"freebreakfast" : 1},
         24: {"photo" : "panpacific.png", "name" : "Park Regis Singapore","location" : "Clarke Quay","price" : 170,"hotelclass" : 4,"guestreview" : 8.2,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 1},
         25: {"photo" : "panpacific.png", "name" : "Holiday Inn Express Clarke Quay","location" : "Clarke Quay","price" : 168,"hotelclass" : 3,"guestreview" : 8.5,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 1},
-        26: {"photo" : "panpacific.png", "name" : "Paradox Singapore Merchant Court at Clarke Quay","location" : "Clarke Quay","price" : 192,"hotelclass" : 4,"guestreview" : 9.0,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 1},
+        26: {"photo" : "panpacific.png", "name" : "Paradox Singapore Merchant Court","location" : "Clarke Quay","price" : 192,"hotelclass" : 4,"guestreview" : 9.0,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 1},
         27: {"photo" : "panpacific.png", "name" : "Studio M Hotel","location" : "Clarke Quay","price" : 128,"hotelclass" : 4,"guestreview" : 7.7,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 1},
         28: {"photo" : "panpacific.png", "name" : "Peninsula Excelsior Hotel","location" : "Clarke Quay","price" : 172,"hotelclass" : 4,"guestreview" : 7.7,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 0},
         29: {"photo" : "panpacific.png", "name" : "M Social Singapore","location" : "Clarke Quay","price" : 128,"hotelclass" : 4,"guestreview" : 8.1,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 1},
@@ -49,7 +49,7 @@ def main_screen(root):
         39: {"photo" : "panpacific.png", "name" : "Hotel Calmo Bugis","location" : "Bugis","price" : 96,"hotelclass" : 3,"guestreview" : 7.5,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 0},
         40: {"photo" : "panpacific.png", "name" : "Hotel Nuve Heritage","location" : "Bugis","price" : 164,"hotelclass" : 4,"guestreview" : 8.2,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 1},
         41: {"photo" : "panpacific.png", "name" : "Pan Pacific Serviced Suites Beach Road","location" : "Bugis","price" : 313,"hotelclass" : 5,"guestreview" : 8.7,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 1},
-        42: {"photo" : "panpacific.png", "name" : "Wanderlust, The Unlimited Collection by Oakwood","location" : "Little India","price" : 156,"hotelclass" : 4,"guestreview" : 9.0,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 0},
+        42: {"photo" : "panpacific.png", "name" : "Wanderlust, Oakwood","location" : "Little India","price" : 156,"hotelclass" : 4,"guestreview" : 9.0,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 0},
         43: {"photo" : "panpacific.png", "name" : "The Daulat by Hotel Calmo","location" : "Little India","price" : 92,"hotelclass" : 3,"guestreview" : 7.1,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 0},
         44: {"photo" : "panpacific.png", "name" : "The Great Madras","location" : "Little India","price" : 66,"hotelclass" : 4,"guestreview" : 7.4,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 1},
         45: {"photo" : "panpacific.png", "name" : "ST Signature Jalan Besar","location" : "Little India","price" : 88,"hotelclass" : 3,"guestreview" : 8.1,"shuttlebusservice" : 0,"spa" : 0,"freebreakfast" : 0},
@@ -64,7 +64,18 @@ def main_screen(root):
     screen.configure(bg="#d7dae2")
     screen.title("Hotel List")
 
+    framez = Frame(screen)
+    framez.pack(fill=BOTH, expand=1)
+
+    my_canvas = Canvas(framez)
+    my_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+
+    my_scrollbar = Scrollbar(framez, orient=VERTICAL, command=my_canvas.yview)
+    my_scrollbar.pack(side=RIGHT, fill=Y)
+    my_canvas.configure(yscrollcommand=my_scrollbar.set, scrollregion=(0,0,0,15000))
+
     
+
     counter=28
     for x, hotel in hotelmain.items():
         hotelImage = hotel["photo"]
@@ -86,37 +97,38 @@ def main_screen(root):
             spa = 'Yes'
         else:
             spa = 'No'
-    
-        hotel1=Image.open(hotelImage)
-        hotel1=hotel1.resize((400,260),Image.ANTIALIAS)
-        photo_logo1=ImageTk.PhotoImage(hotel1)
+        
 
-        hotel1_frame=Frame(root,bd=5,relief=RIDGE)
-        hotel1_frame.place(x=30,y=counter,width=820,height=4000)
 
-        placement1=Label(hotel1_frame,image=photo_logo1,compound=LEFT)
-        placement1.place(x=10,y=counter)
+        img = Image.open(hotelImage)
+        img = img.resize((390, 227), Image.ANTIALIAS)  
+        photo = ImageTk.PhotoImage(img)
+
+        hotel["photo_obj"] = photo
+
+        hotel1_frame=Frame(my_canvas,bd=5,relief=RIDGE)
+        hotel1_frame.place(x=30,y=counter,width=600,height=4000)
+
+        placement1=Label(hotel1_frame,image=photo,compound=LEFT)
+        placement1.place(x=10,y=10)
 
 
         hotelname=Label(hotel1_frame,text=hotelnamex, font=('times new roman',18,'bold'),fg='darkblue')
-        hotelname.grid(row=0,column=0,padx=430,pady=20,sticky=W)
+        hotelname.grid(row=0,column=0,padx=(430, 0), pady=20, sticky=W)
         info=Label(hotel1_frame,text='Price: $' + str(price), font=('times new roman',12,'bold'))
-        info.grid(row=1,column=0,padx=430,pady=5,sticky=W)
+        info.grid(row=1,column=0,padx=(430, 0),pady=5,sticky=W)
         info=Label(hotel1_frame,text='Hotel Rating: ' + str(rating), font=('times new roman',12,'bold'))
-        info.grid(row=2,column=0,padx=430,pady=5,sticky=W)
+        info.grid(row=2,column=0,padx=(430, 310),pady=5,sticky=W)
         info=Label(hotel1_frame,text='Breakfast Availability: ' + breakfast, font=('times new roman',12,'bold'))
-        info.grid(row=3,column=0,padx=430,pady=5,sticky=W)
+        info.grid(row=3,column=0,padx=(430, 0),pady=5,sticky=W)
         info=Label(hotel1_frame,text='Shuttle Service Availability: ' + bus, font=('times new roman',12,'bold'))
-        info.grid(row=4,column=0,padx=430,pady=5,sticky=W)
+        info.grid(row=4,column=0,padx=(430, 0),pady=5,sticky=W)
         info=Label(hotel1_frame,text='Spa Availability: ' + spa, font=('times new roman',12,'bold'))
-        info.grid(row=5,column=0,padx=430,pady=5,sticky=W)
+        info.grid(row=5,column=0,padx=(430, 0),pady=5,sticky=W)
 
+        my_canvas.create_window(10, counter, anchor=NW, window=hotel1_frame)
         counter+=300
 
-        scrollbar=ttk.Scrollbar(screen,orient='vertical')
-        scrollbar.place(relx=1,rely=0,relheight=1,anchor='ne')
- 
-        screen.bind('<MouseWheel>', lambda event: screen.yview_scroll(20,"units"))
     
     screen.mainloop()
     
